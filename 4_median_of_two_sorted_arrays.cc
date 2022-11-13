511a006b21539637a0b0396771a40583b4c2e454
jinbeom-kim-kr <jinbeom.kim.kr@gmail.com>
2022-11-13 18:42:34 +0900

    try agin

---
+++ 4_median_of_two_sorted_arrays.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <climits>
+
+class Solution {
+public:
+    double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if (n1 < n2) findMedianSortedArrays(nums2, nums1);
+
+        int lo = 0;
+        int hi = n2 * 2;
+        while (lo <= hi) {
+           int mid2 = (lo + hi) / 2;
+           int mid1 = n1 + n2 - mid2;
+
+           double l1 = (mid1 == 0) ? INT_MIN : nums1[(mid1-1)/2];
+           double l2 = (mid2 == 0) ? INT_MIN : nums2[(mid2-1)/2];
+           double r1 = (mid1 == n1 * 2) ? INT_MAX : nums1[(mid1)/2];
+           double r2 = (mid2 == n2 * 2) ? INT_MAX : nums2[(mid2)/2];
+
+           if (l1 > r2) lo = mid2 + 1;
+           else if (l2 > r1) hi = mid2 - 1;
+           else return (std::max(l1, l2) + std::min(r1, r2)) / 2;
+        }
+        return -1;
+    }
+};
